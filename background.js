090b8c62d44396c343ded8822e2da6aa48089919
Hadi Khan <khan.hadi2951@gmail.com>
2023-03-12 00:51:55 +0500

    added basic error handling

--- background.js
+++ background.js
@@ -1,13 +1,18 @@
 function handleClick() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {message: "set_cookie"}, function(response) {
-      if (response && response.message === "cookie_set") {
-        console.log("Cookie set successfully.");
-      } else {
-        console.log("Cookie not set.");
-      }
+  try {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { message: "set_cookie" }, function (response) {
+        if (response && response.message === "cookie_set") {
+          console.log("Cookie set successfully.");
+        } else {
+          console.log("Cookie not set.");
+        }
+      });
     });
-  });
+  }
+  catch (error) {
+    console.log("Erorr in set cookie extension:" + error);
+  }
 }
 
 chrome.action.onClicked.addListener(handleClick);
--- content.js
+++ content.js
@@ -1,13 +1,18 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.message === "set_cookie") {
-    var cookieName = "test";
-    var cookieValue = "12345";
-    var url = window.location.hostname;
-    console.log(url);
-    var expires = new Date();
-   expires.setTime(expires.getTime() + (48 * 60 * 60 * 1000)); 
-   document.cookie = cookieName + "=" + cookieValue + ";expires=" + expires.toUTCString() + ";path=/;domain="+ url + ";SameSite=None;Secure";
-   location.reload();
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  try {
+    if (request.message === "set_cookie") {
+      var cookieName = "test";
+      var cookieValue = "12345";
+      var url = window.location.hostname;
+      console.log(url);
+      var expires = new Date();
+      expires.setTime(expires.getTime() + (48 * 60 * 60 * 1000));
+      document.cookie = cookieName + "=" + cookieValue + ";expires=" + expires.toUTCString() + ";path=/;domain=" + url + ";SameSite=None;Secure";
+      location.reload();
+    }
+  }
+  catch (error) {
+    console.log("Error in set cookie extension: " + error);
   }
 });
 
